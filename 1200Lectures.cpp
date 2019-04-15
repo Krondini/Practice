15a45d733f38ecadc30dd05973ed8a1eb87d4ef2
Krondini <koro5940@colorado.edu>
2019-04-15 14:48:09 -0600

    Yes

--- 1200Lectures.cpp
+++ 1200Lectures.cpp
@@ -4,22 +4,45 @@ Cat Rondini
 */
 
 #include <iostream>
-#include <cmath>
+#include <limits.h>
 
 using namespace std;
 
-int calculate_song_storage(float GB)
+// int calculate_song_storage(float GB)
+// {
+// 	float size_in_MB = GB * 1024;
+// 	size_in_MB /= 3.5;
+// 	return floor(size_in_MB);
+// }
+
+int minDistance(int dist[], bool sptSet[])
 {
-	float size_in_MB = GB * 1024;
-	size_in_MB /= 3.5;
-	return floor(size_in_MB);
+	//Init min value
+	int min = INT_MAX, min_index;
+
+	for (int v = 0; v < V; v++)
+	{
+		if(!sptSet[v] && dist[v] <= min)
+		{
+			min = dist[v];
+			min_index = v;
+		}
+	}
+	return min_index;
+}
+
+int printSolution(int dist[], int n)
+{
+	printf("Vertex\t Distance from Source\n");
+	for(int i = 0; i < V; i++)
+		printf("%d tt %d\n", i, dist[i]);
 }
 
 int main(int argc, char const *argv[])
 {
-	float MP3_size = stof(argv[1]);
-	int num_songs = calculate_song_storage(MP3_size);
-	printf("You can store %d songs on this MP3.\n", num_songs);
+	// float MP3_size = stof(argv[1]);
+	// int num_songs = calculate_song_storage(MP3_size);
+	// printf("You can store %d songs on this MP3.\n", num_songs);
 	// cout << "The address of the variable 'MP3_size' is: " << &MP3_size << endl;
 	return 0;
 }
