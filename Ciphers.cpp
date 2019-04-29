08925aded5bffc527b48953b4d2534802e230db2
Krondini <koro5940@colorado.edu>
2019-04-29 13:54:26 -0600

    Started on Matrix Cipher

--- Ciphers.cpp
+++ Ciphers.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <cctype>
+#include <vector>
 
 using namespace std;
 
@@ -9,9 +11,8 @@ void menu()
 		 <<"you would like to use: \n";
 	printf("1.) Caesar Cipher\n");
 	printf("2.) Vigenere Cipher\n");
+	printf("3.) Matrix Cipher\n");
 	printf("0.) Exit\n");
-
-	return;
 }
 
 
@@ -77,38 +78,75 @@ string VigenereCipher(string input, string key)
 	return result;
 }
 
+string MatrixCipher(string input, int size)
+{
+	//Declare Matrix
+	char EncryptMatrix[input.length()][input.length()];
+	//Initialize matrix values
+	for(int i = 0; i < input.length(); i++) 
+	{
+		for(int j = 0; j < input.length(); j++)
+			EncryptMatrix[i][j] = 0;
+	}
+}
+
+/* Takes in a file name
+* Decodes a file with several encoded message
+* Returns the original plaintext
+*/
+string Decoder(string filename)
+{
+	ifstream fo;
+	fo.open(filename);
+	char data[100];
+
+	fo >> data; //Read first line
+}
+
 int main(int argc, char const *argv[])
 {
 
 	printf("Please input the word/phrase you wish to send\n");
-	char input[100];
-	cin.getline(input, sizeof(input));
+	string input;
+	getline(cin, input);
+	//cout << "Input is: " << input << endl;
+
+	ofstream fo;
+	fo.open("CodesToDecode.txt", ofstream::app);
 	cout << "\n\n";
 	int choice = -1;
 	while(choice != 0)
 	{
 		menu();
 		cin >> choice;
-		if(choice == 1)
+		if(choice == 1) //Caesar Cipher
 		{
 			cout << "Please enter how far you would like to "
 				 << "shift your message\n";
 			int shift;
 			cin >> shift;
+			string Encrypted = CaesarCipher(input, shift);
 			cout << "Plaintext: " << input;
 			printf("\nShift: %d\n", shift);
-			cout << "Encrypted: " << CaesarCipher(input, shift) << endl;			
+			cout << "Encrypted: " << Encrypted << endl;
+			fo << Encrypted << endl;
 		}
-		else if(choice == 2)
+		else if(choice == 2) //Vigenere Cipher
 		{
 			printf("Please enter your key word:\n");
 			string key;
 			cin >> key;
+			string Encrypted = VigenereCipher(input, key);
 			cout << "\n\nPlaintext: " << input;
 			cout << "\nKey: " << key;
-			cout << "\nEncrypted: " << VigenereCipher(input, key) << endl;
+			cout << "\nEncrypted: " << Encrypted << endl;
+			fo << Encrypted << endl;
+		}
+		else if(choice == 3) //Matrix Cipher
+		{
+
 		}
 	}
-
+	fo.close();
 	return 0;
 }
