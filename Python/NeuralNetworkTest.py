ab578dcf8b499b13461f903edc4a4d3fea0f4a5d
Krondini <kjrondini@gmail.com>
2019-05-15 10:52:59 -0700

    Created Sigmoid function

--- Python/NeuralNetworkTest.py
+++ Python/NeuralNetworkTest.py
@@ -1,8 +1,17 @@
 import numpy as np
 import sys
 
+def sigmoid(x):
+	return (1/(1+np.exp(-x)))
+
+def neural(in1, in2, w1, w2, bias):
+	out = (w1*in1) + (w2*in2) + bias
+	return sigmoid(z)
+
 def main(argv):
 	print(argv[0])
+	test_in1 = np.random.randn()
+	test_in1 = 
 
 if __name__ == '__main__':
 	main(sys.argv)
