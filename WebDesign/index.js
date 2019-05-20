d9e44f0d249fb7f6d664219d3b710f0855ba08fb
Krondini <kjrondini@gmail.com>
2019-05-20 11:17:19 -0700

    Testing out JavaScript events

---
+++ WebDesign/index.js
@@ -0,0 +1,11 @@
+function changeItem(id)
+{
+	var item = document.getElementById(id);
+	item.innerHTML = "Hello World!";
+}
+
+function changeItem2(id)
+{
+	var item = document.getElementById(id);
+	item.innerHTML = "Goodbye World!";	
+}
