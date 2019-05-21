325951027e11463593e4e503871e894e5c258e3b
Krondini <kjrondini@gmail.com>
2019-05-20 21:08:21 -0700

    Removed JavaScript file, implanting script directly in HTML

--- WebDesign/Games/Snake/index.js
+++ WebDesign/Games/Snake/index.js
@@ -5,6 +5,26 @@ function startGame(){
 	myGamePiece = new component(30, 30, 'red', 10, 120);
 }
 
+var myGameArea = {
+	canvas: document.createElement('canvas'),
+	start: function(){
+		this.canvas.width = 480;
+		this.canvas.height = 270;
+		this.context = this.canvas.getContext('2d');
+		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+		this.interval = setInterval(updateGameArea, 20);
+		window.addEventListener('keydown', function(e) {
+			myGameArea.key = e.keyCode;
+		})
+		window.addEventListener('keyup', function(e) {
+			myGameArea.key = false;
+		})
+	},
+	clear: function(){
+		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	}
+}
+
 function component(width, height, color, x, y){
 	this.width = width;
 	this.height = height;
@@ -45,22 +65,3 @@ function stopMove() {
 	myGamePiece.speedX = 0;
 }
 
-var myGameArea = {
-	canvas: document.createElement('canvas'),
-	start: function(){
-		this.canvas.width = 480;
-		this.canvas.height = 270;
-		this.context = this.canvas.getContext('2d');
-		document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-		this.interval = setInterval(updateGameArea, 20);
-		window.addEventListener('keydown', function(e) {
-			myGameArea.key = e.keyCode;
-		})
-		window.addEventListener('keyup', function(e) {
-			myGameArea.key = false;
-		})
-	},
-	clear: function(){
-		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-	}
-}
