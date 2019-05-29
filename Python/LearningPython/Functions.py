be313127b5a0e900496a7e97721a24690e7fe223
Krondini <kjrondini@gmail.com>
2019-05-29 14:42:55 -0700

    Digital Root Problem

--- Python/LearningPython/Functions.py
+++ Python/LearningPython/Functions.py
@@ -7,9 +7,17 @@ def rectangle(n,m):
 		print('')
 
 def addExcitement1(list_of_strings):
-	for item in list_of_strings:
-		item += '!'
+	for item in range(len(list_of_strings)):
+		list_of_strings[item] += '!'
 	return list_of_strings
+
+def digitalRoot(n):
+	if len(str(n)) == 1:
+		return n
+	answer = 0
+	for digit in str(n):
+		answer += int(digit)
+	return digitalRoot(answer)
 
 def main():
 	#Task 1
@@ -28,9 +36,17 @@ def main():
 
 	print("Before: ", end='')
 	print(list_of_strings)
+
 	list_of_strings = addExcitement1(list_of_strings)
+	
 	print("\nAfter: ", end='')
 	print(list_of_strings)
+
+	#Task 3
+	n_digitalRoot = int(input("Please enter a number to find the Digital Root of: "))
+	answer = digitalRoot(n_digitalRoot)
+	
+	print("Digital Root of %d is: %d"%(n_digitalRoot,answer))
 	return 0
 
 if __name__ == '__main__':
