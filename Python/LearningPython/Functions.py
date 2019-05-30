de091dcb162893a244800b0b07f36428b69cbaed
Krondini <kjrondini@gmail.com>
2019-05-30 09:44:29 -0700

    Added first_diff() and binom()

--- Python/LearningPython/Functions.py
+++ Python/LearningPython/Functions.py
@@ -1,4 +1,6 @@
 import random
+import os
+import math
 
 def rectangle(n,m):
 	for rows in range(n):
@@ -19,12 +21,34 @@ def digitalRoot(n):
 		answer += int(digit)
 	return digitalRoot(answer)
 
+def first_diff(str1, str2):
+	if(str1 == str2):
+		return -1
+	else:
+		i = 0
+		for i in range(len(str1)):
+			if(str1[i] != str2[i]):
+				return i+1
+			else:
+				continue
+		return i+1
+
+def binom(n, k):
+	#binomial coefficient defined as (n!/(k!*(n-k)!))
+	return (math.factorial(n)/(math.factorial(k)*(math.factorial(n-k))))
+
 def main():
 	#Task 1
 	n_rectangle = int(input("Please enter the number of rows\nfor function 'rectangle': "))
 	m_rectangle = int(input("Please enter the number of columns\nfor function 'rectangle': "))
+	os.system("clear")
+	print("Here is a %dx%d rectangle:"%(n_rectangle,m_rectangle))
 	rectangle(n_rectangle,m_rectangle)
-	print('')
+	
+	while True:
+		waiting = input("> ")
+		if waiting == "":break
+	os.system("clear")
 
 	#Task 2
 	list_of_strings = []
@@ -34,6 +58,7 @@ def main():
 		if(new_item == ''):
 			break
 		list_of_strings.append(new_item)
+	os.system("clear")
 
 	print("Before: ", end='')
 	print(list_of_strings)
@@ -43,12 +68,51 @@ def main():
 	print("\nAfter: ", end='')
 	print(list_of_strings)
 
+	while True:
+		waiting = input("> ")
+		if waiting == "":break
+	os.system("clear")
+
 	#Task 3
 	n_digitalRoot = int(input("Please enter a number to find the Digital Root of: "))
 	answer = digitalRoot(n_digitalRoot)
+	os.system("clear")
 	
 	print("Digital Root of %d is: %d" % (n_digitalRoot,answer))
+	while True:
+		waiting = input("> ")
+		if waiting == "":break
+	os.system("clear")
 
+	#Task 4
+	str1 = input("Please enter the first string to check: ")
+	str2 = input("\nPlease enter the second string to check: ")
+	os.system("clear")
+
+	diff_check = first_diff(str1, str2)
+	if(diff_check == -1):
+		print("The strings are exactly the same!")
+	else:
+		print("The strings differ at position %d" % diff_check)
+
+	while True:
+		waiting = input("> ")
+		if waiting == "":break
+	os.system("clear")
+
+	#Task 5
+	print("Calculating binomial coefficient: ")
+	int1 = int(input("Please enter the first integer n: "))
+	int2 = int(input("Please enter the second integer k: "))
+	os.system("clear")
+
+	binom_coeff = binom(int1, int2)
+	print("Binomial Coefficient for %d and %d is: %d" % (int1, int2, binom_coeff))
+
+	while True:
+		waiting = input("> ")
+		if waiting == "":break
+	os.system("clear")
 
 	return 0
 
