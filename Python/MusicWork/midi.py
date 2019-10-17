74dc2cec2552e36a44a43cf91806e307eceb1caa
Krondini <koro5940@colorado.edu>
2019-10-17 14:02:53 -0600

    Converted a midi to csv

--- Python/MusicWork/midi.py
+++ Python/MusicWork/midi.py
@@ -1,4 +1,10 @@
 import py_midicsv
+import sys
 
-def convert_midi_csv(midi_file):
-	pass
+def main(argv):
+	midi_fo = py_midicsv.midi_to_csv(argv[1]) #This is the midi file to convert
+	for i in range(1000, 1100):
+		print(midi_fo[i])
+
+if __name__ == '__main__':
+	main(sys.argv)
