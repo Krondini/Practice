5ca0bb802e93d0c15469122206337fab472b7825
Krondini <koro5940@colorado.edu>
2019-10-21 11:53:29 -0600

    Removed files

--- Python/MusicWork/midi.py
+++ Python/MusicWork/midi.py
@@ -3,8 +3,8 @@ import sys
 
 def main(argv):
 	midi_fo = py_midicsv.midi_to_csv(argv[1]) #This is the midi file to convert
-	for i in range(1000, 1100):
-		print(midi_fo[i])
+	for line in midi_fo:
+		print(line)
 
 if __name__ == '__main__':
 	main(sys.argv)
