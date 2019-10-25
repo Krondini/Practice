8392c9bfc2286a7ceedf23fc040b94844b49127f
Krondini <koro5940@colorado.edu>
2019-10-25 10:00:40 -0600

    Splitting blank midi

--- Python/MusicWork/midi.py
+++ Python/MusicWork/midi.py
@@ -4,7 +4,9 @@ import sys
 def main(argv):
 	midi_fo = py_midicsv.midi_to_csv(argv[1]) #This is the midi file to convert
 	for line in midi_fo:
+		line = line.strip().split(", ")
 		print(line)
+
 
 if __name__ == '__main__':
 	main(sys.argv)
