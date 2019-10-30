70a52b94ce2870fc2b305967f7e2187af5496e84
Krondini <koro5940@colorado.edu>
2019-10-30 10:47:13 -0600

    Update

--- Bash/moveMidis.sh
+++ Bash/moveMidis.sh
@@ -2,8 +2,17 @@
 FilesFromMuseScore=~/Documents/MuseScore2/Scores/*
 FolderForScript=~/Documents/Practice/Python/MusicWork/
 
+
+# Move files from MuseScore to processing
 for file in $FilesFromMuseScore
 do
-	echo "Processing file $file ..."
+	echo "Moving file $file to processing..."
 	cp $file $FolderForScript
+done
+
+# Process midis
+for file in $FolderForScript.mid
+do
+	echo "Processing $file..."
+	python3 
 done
