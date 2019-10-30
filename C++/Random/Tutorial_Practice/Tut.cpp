5695fed2eec7ec697cc973656b67f98ed42da47f
Krondini <koro5940@colorado.edu>
2019-10-29 20:07:25 -0400

    Added folders

---
+++ C++/Random/Tutorial_Practice/Tut.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numeric>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello World!\n";
+
+	return 0;
+}
