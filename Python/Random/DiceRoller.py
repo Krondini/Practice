bf752f3f4653b4024bf83914668b2a8a0f651685
Krondini <koro5940@colorado.edu>
2019-12-26 21:29:36 -0700

    General Update

--- Python/Random/DiceRoller.py
+++ Python/Random/DiceRoller.py
@@ -5,26 +5,45 @@ Purpose: Short code to quickly roll dice for D&D characters
 '''
 
 from random import randint
+from sys import argv
 
 def rollDice(num_dice, num_sides):
 	
 	result = []
 	for i in range(num_dice):
-		result.append(randint(1, num_sides))
+		result.append(randint(2, num_sides))
 
 	result = sorted(result, reverse=True)
+
 	return result
 
-def main():
+def main(args):
 	
-	num_dice = int(input("Please enter how many dice you wish to roll: "))
-	num_sides = int(input("Please enter how many sides these dice have: "))
+	char_roll = input("Is this a character roll? (y/n) ")
 
-	list_of_rolls = rollDice(num_dice, num_sides)
+	if char_roll.lower() == 'n': #Random roll
+		num_dice = int(input("Please enter how many dice you wish to roll: "))
+		num_sides = int(input("Please enter how many sides these dice have: "))
 
-	print(list_of_rolls)
+		list_of_rolls = rollDice(num_dice, num_sides)
+
+		print(list_of_rolls)
+
+	else: #Character roll
+		six_rolls = []
+		for i in range(6):
+		
+			new_roll = rollDice(4, 6)
+			del new_roll[-1]
+			roll_sum = sum(new_roll)
+			six_rolls.append(roll_sum)
+
+		six_rolls = sorted(six_rolls, reverse=True)
+
+		print(six_rolls)
+
 
 	return 0
 
 if __name__ == '__main__':
-	main()
+	main(argv)
